a16963cfc4706b5c61b1b024f2556eecc192f396
Синюкович Никита Андреевич <mikita.siniukovich@softclub.by>
2023-12-28 17:15:44 +0300

    edi_test

--- src/test/java/by/softclub/edi/authorization/LoginPage.java
+++ src/test/java/by/softclub/edi/authorization/LoginPage.java
@@ -2,12 +2,14 @@ package by.softclub.edi.authorization;
 
 import by.softclub.edi.authorization.implementation.Implementation;
 import io.qameta.allure.Step;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 import static by.softclub.edi.testBase.Locators.getLocator;
 
 public class LoginPage extends Implementation {
 
+    Logger LOG = Logger.getLogger(LoginPage.class);
     WebDriver driver;
 
     public LoginPage(WebDriver driver) {
@@ -16,6 +18,7 @@ public class LoginPage extends Implementation {
 
     @Step("Entering credentials, clicking login button")
     public void login(String username, String password) throws Exception {
+        LOG.info("Attempting to login with credentials:" + username + " " + password);
         driver.findElement(getLocator("loginPage.loginInput")).sendKeys(username);
         driver.findElement(getLocator("loginPage.passwordInput")).sendKeys(password);
         driver.findElement(getLocator("loginPage.loginButton")).click();
@@ -23,7 +26,9 @@ public class LoginPage extends Implementation {
 
     @Step("ChangeVisibility")
     public void changeVisibility() throws Exception {
+        LOG.info("Changing text's visibility");
         driver.findElement(getLocator("loginPage.changeVisibility")).click();
+        LOG.info("Visibility has changed");
     }
 
 //    public String getText(String locator) throws Exception {
--- src/test/java/by/softclub/edi/authorization/RestorePage.java
+++ src/test/java/by/softclub/edi/authorization/RestorePage.java
@@ -2,12 +2,14 @@ package by.softclub.edi.authorization;
 
 import by.softclub.edi.authorization.implementation.Implementation;
 import io.qameta.allure.Step;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 import static by.softclub.edi.testBase.Locators.getLocator;
 
 public class RestorePage extends Implementation {
 
+    Logger LOG = Logger.getLogger(LoginPage.class);
     WebDriver driver;
 
     public RestorePage(WebDriver driver) {
@@ -20,6 +22,7 @@ public class RestorePage extends Implementation {
 
     @Step("Click to restore button")
     public void redirectToRestorePage() throws Exception {
+        LOG.info("Click to \"Forgot password\" button");
         driver.findElement(getLocator("loginPage.forgotPasswordButton")).click();
     }
 }
